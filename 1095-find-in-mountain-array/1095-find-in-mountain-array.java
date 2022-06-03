a41d94798be6ad40be0178f34f7e2a4b322c8e78
Santhosh Kumar Thoomatti <thoomatti.santhosh.w@gmail.com>
2022-06-03 17:00:35 +0530

    Time: 0 ms (100.00%), Space: 43.7 MB (44.98%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -0,0 +1,79 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+     
+        
+        int maxIndex = peak(mountainArr);
+
+        int index = searchAsc(mountainArr, target, 0 , maxIndex);
+        if(index == -1){
+            index = searchDsc(mountainArr, target, maxIndex, mountainArr.length() - 1);
+        }
+        
+        return index;
+    }
+    
+    public int peak(MountainArray arr){
+        int start = 0;
+        int end = arr.length() - 1;
+        
+        while(start < end){
+            int mid = start + (end - start) / 2;
+            
+            if(arr.get(mid) > arr.get(mid + 1)){
+                end = mid;
+            } else {
+                start = mid + 1;
+            }            
+        }
+        return start;
+    }
+    
+    public int searchAsc(MountainArray arr, int n, int s, int e){
+        int start = s;
+        int end = e;
+        
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            
+            if(arr.get(mid) == n){
+                return mid;
+            }
+            
+            if(arr.get(mid) > n){
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }            
+        }
+        return -1;
+    }
+    
+    public int searchDsc(MountainArray arr, int n, int s, int e){
+        int start = s;
+        int end = e;
+        
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            
+            if(arr.get(mid) == n){
+                return mid;
+            }
+            
+            if(arr.get(mid) < n){
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }            
+        }
+        return -1;
+    }
+}
