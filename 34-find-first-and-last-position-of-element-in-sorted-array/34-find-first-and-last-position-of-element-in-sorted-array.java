f3baa00eae23d4e74f1b23d94bb0ac2a7fc78f2a
Santhosh Kumar Thoomatti <thoomatti.santhosh.w@gmail.com>
2022-06-03 12:06:28 +0530

    Time: 1 ms (31.81%), Space: 47.2 MB (45.07%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,46 +1,44 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        
-        int index = search(nums, target);
-        if(index == -1){
-            return new int[]{-1,-1};
-        }
-        
-        int start = index;
-        int end = index;
-                
-        while(start > 0 && nums[start - 1] == target){
-            start = start - 1;
-        }
-        
-        while(end < nums.length - 1 && nums[end + 1] == target){
-            end = end + 1;
-        }
-        
-        return new int[]{start, end};
-        
-    }
+public int[] searchRange(int[] arr, int target){
+        int ceil = ceiling(arr, target-1);
+        int floor = floor(arr, target+1);
     
-    public int search(int[] nums, int target){
-        int length = nums.length;
+        if(ceil > floor){
+            return new int[]{-1, -1};
+        }        
+        
+        return new int[]{ceil, floor};
+    }
+
+    public int ceiling(int[] arr, int target) {
         int start = 0;
-        int end = length - 1;
+        int end  = arr.length - 1;
         
         while(start <= end){
-            
-            int mid = start + (end - start) / 2;
-            
-            if(nums[mid] == target){
-                return mid;
-            }
-            
-            if(nums[mid] > target){
+            int mid = start + (end - start)/2;
+
+            if(target < arr[mid]){
                 end = mid - 1;
             } else {
                 start = mid + 1;
-            }            
+            }
         }
-        
-          return -1;
+        return start;
+    }
+
+    public int floor(int[] arr, int target) {
+        int start = 0;
+        int end  = arr.length - 1;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(target > arr[mid]){
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return end;
     }
 }
