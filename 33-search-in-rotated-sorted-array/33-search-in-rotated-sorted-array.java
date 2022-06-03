043cb948e908142831e0c323ca410df00884b492
Santhosh Kumar Thoomatti <thoomatti.santhosh.w@gmail.com>
2022-06-03 19:57:30 +0530

    Time: 1 ms (61.76%), Space: 42.6 MB (42.80%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -23,7 +23,7 @@ class Solution {
         int start = 0;
         int end = arr.length - 1;
 
-        while(start < end){
+        while(start <= end){
             int mid = start + (end - start) / 2;
 
             if(arr[mid] > arr[mid+1]){
