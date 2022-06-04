14899b3629d168236df167221b2a9fc9b59137cd
Santhosh Kumar Thoomatti <thoomatti.santhosh.w@gmail.com>
2022-06-04 12:28:42 +0530

    Time: 1 ms (39.45%), Space: 43.4 MB (42.06%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -4,12 +4,16 @@ class Solution {
         int maxSum = 0;
         for(int i = 0; i < accounts.length; i++){
             int sum = 0;
-            for(int j = 0; j < accounts[i].length; j++){            
-                sum += accounts[i][j];                
+            for(int j = 0; j < accounts[i].length; j++){
+            
+                sum += accounts[i][j];
+                
             }
+            
             if(sum > maxSum){
                 maxSum = sum;
-            }            
+            }
+            
         }
         
         return maxSum;
