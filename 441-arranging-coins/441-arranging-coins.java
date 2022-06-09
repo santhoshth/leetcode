3cf30620892130298fc70b6edd79a61530fff463
Santhosh Kumar Thoomatti <thoomatti.santhosh.w@gmail.com>
2022-06-09 15:19:54 +0530

    Time: 6 ms (39.36%), Space: 40.9 MB (67.63%) - LeetHub

---
+++ 441-arranging-coins/441-arranging-coins.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        if(n==0) return 0;
+        int i = 0;
+        while(n > 0){
+            n = n - i;
+            i++;
+        }
+
+        if (n==0) return i-1;
+        else return i-2;
+    }
+}
