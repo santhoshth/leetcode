fde18fdb4baef2ff0b608ccc61532e63c8dbe102
Santhosh Kumar Thoomatti <thoomatti.santhosh.w@gmail.com>
2022-06-09 15:27:45 +0530

    Time: 2 ms (93.12%), Space: 40.8 MB (69.73%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.java
+++ 441-arranging-coins/441-arranging-coins.java
@@ -1,6 +1,10 @@
 class Solution {
     public int arrangeCoins(int n) {
-        return (int) (Math.sqrt(2* (long)n + 0.4) - 0.5);
+        
+        double ans = Math.sqrt(2*(long)n + 0.4);
+        int res = (int)(ans - 0.5);
+        
+        return res;
 //         if(n==0) return 0;
 //         int i = 0;
 //         while(n > 0){
