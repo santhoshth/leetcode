f59d533322e10425dc14501c12863b33346d0eab
Santhosh Kumar Thoomatti <thoomatti.santhosh.w@gmail.com>
2022-06-13 11:42:58 +0530

    Time: 1 ms (67.99%), Space: 43.6 MB (15.38%) - LeetHub

---
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null && list2 != null) {
+            return list2;
+        }
+
+        if (list1 != null && list2 == null) {
+            return list1;
+        }
+
+        if (list1 == null && list2 == null) {
+            return null;
+        }
+        
+        ListNode result = list1.val <= list2.val ? list1 : list2;
+        
+        if(list1.val <= list2.val)
+            result = merge(list1.next,list2, result);
+        else
+            result = merge(list1,list2.next, result);
+        
+        
+        
+        return result;
+    }
+    
+    public ListNode merge(ListNode one, ListNode two, ListNode result){
+        if(result == null){
+            return result;
+        }
+        
+        boolean good = valid(one) && valid(two);
+        
+        if(good && one.val <= two.val){
+            result.next = one;
+            one = one.next;
+        }
+        
+        else if(good && one.val > two.val){
+            result.next = two;
+            two = two.next;            
+        }
+        
+        if(!good && one != null){
+            result.next = one;
+            one = one.next;
+        }
+        
+        if(!good && two != null){
+            result.next = two;
+            two = two.next;    
+        }
+        
+        result.next = merge(one, two, result.next);
+        
+        return result;
+    }
+    
+    public boolean valid(ListNode node){
+        return node != null;
+    }
+}
