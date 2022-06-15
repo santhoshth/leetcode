47be66ee45828bb0c81d8af6823091bda9e53856
Santhosh Kumar Thoomatti <thoomatti.santhosh.w@gmail.com>
2022-06-15 13:30:15 +0530

    Time: 4 ms (29.48%), Space: 47.9 MB (37.45%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -9,31 +9,25 @@
  * }
  */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {        
-//         if(l1 == null && l2 == null){
-//             return l1;
-//         }
-        
-//         if(l1 == null && l2 != null){
-//             return l2;
-//         }
-        
-//         if(l1 != null && l2 == null){
-//             return l1;
-//         }
-        
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {                
         ListNode res = add(l1, l2, 0);
         
         return res;
     }
     
     public ListNode add(ListNode i, ListNode j, int carry){
+        // when both node are null, return based on carry value
+        // if carry is 0 return null else return the carry value as node
+        
         if(i == null && j == null){
             return carry == 1 ? new ListNode(carry) : i;
         }
         
+        // Value is calculated by sum of 3 values
+        // i.val + j.val + carry        
         int val = 0;
         
+        // when any one node is null, skip the null node
         if(i == null && j != null){
             val = j.val + carry;
         }
@@ -42,12 +36,17 @@ class Solution {
             val = i.val + carry;
         }
         
+        // when both node are valid, add both the values
         if( i != null && j != null){            
             val = (i.val + j.val + carry);
         }
         
+        // creating a node with calculated value
         ListNode res = new ListNode(val % 10);
         
+        // when both the list have different length
+        // once the shortest list is completed, it can be passed as null
+        // then val will be calculated based on remaining nodes of longest list        
         ListNode iNext = i != null ? i.next : null;
         ListNode jNext = j != null ? j.next : null;
         
