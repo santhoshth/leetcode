6993962f8cd163db38aea126db504d74aa8812b3
Santhosh Kumar Thoomatti <thoomatti.santhosh.w@gmail.com>
2022-06-22 15:22:34 +0530

    Time: 1 ms (99.71%), Space: 44.5 MB (56.63%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,17 +1,42 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> q = new PriorityQueue<Integer>(Collections.reverseOrder());
         
-        for(int each: nums){
-            q.offer(each);
+        quickSort(nums, 0, nums.length-1);
+        
+        if(k <= nums.length) return nums[k-1];
+        
+        return -1;
+    }
+    
+    public void quickSort(int[] arr, int low, int high){
+        if(low >= high){
+            return;
         }
-        
-        int ans = 0;
-        while(k > 0){
-            ans = q.poll();
-            k--;
+
+        int start = low;
+        int end = high;
+        int mid = start + (end - start)/2;
+        int pivot = arr[mid];
+
+        while(start <= end) {
+            while (arr[start] > pivot) {
+                start++;
+            }
+
+            while (pivot > arr[end]) {
+                end--;
+            }
+
+            //swap start and end elements
+            if (start <= end) {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
         }
-        
-        return ans;
+        quickSort(arr, low, end);
+        quickSort(arr, start, high);
     }
 }
