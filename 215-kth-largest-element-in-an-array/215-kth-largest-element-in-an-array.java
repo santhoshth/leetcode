f0cf697031f5279e41c8be17444daaa2d552867c
Santhosh Kumar Thoomatti <thoomatti.santhosh.w@gmail.com>
2022-06-26 08:43:16 +0530

    Time: 10 ms (29.21%), Space: 46.3 MB (6.01%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,41 +1,60 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        // descending sort
-        quickSort(nums, 0, nums.length-1);
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for(int each: nums){
+            maxHeap.offer(each);
+        }
         
-        return nums[k-1];
+        int max = Integer.MAX_VALUE;
+        while(k != 0){
+            max = maxHeap.poll();
+            k--;
+        }
+        
+        
+        return max;
+        
     }
     
-    public void quickSort(int[] arr, int low, int high){
-        if(low >= high){
-            return;
-        }
-
-        int start = low;
-        int end = high;
-        int mid = start + (end - start)/2;
-        int pivot = arr[mid];
-
-        while(start <= end) {
-            while (arr[start] > pivot) {
-                start++;
-            }
-
-            while (pivot > arr[end]) {
-                end--;
-            }
-
-            //swap start and end elements
-            if (start <= end) {
-                int temp = arr[start];
-                arr[start] = arr[end];
-                arr[end] = temp;
-                start++;
-                end--;
-            }
-        }
+    
+    
+//     public int findKthLargest(int[] nums, int k) {
+//         // descending sort
+//         quickSort(nums, 0, nums.length-1);
         
-        quickSort(arr, low, end);
-        quickSort(arr, start, high);
-    }
+//         return nums[k-1];
+//     }
+    
+//     public void quickSort(int[] arr, int low, int high){
+//         if(low >= high){
+//             return;
+//         }
+
+//         int start = low;
+//         int end = high;
+//         int mid = start + (end - start)/2;
+//         int pivot = arr[mid];
+
+//         while(start <= end) {
+//             while (arr[start] > pivot) {
+//                 start++;
+//             }
+
+//             while (pivot > arr[end]) {
+//                 end--;
+//             }
+
+//             //swap start and end elements
+//             if (start <= end) {
+//                 int temp = arr[start];
+//                 arr[start] = arr[end];
+//                 arr[end] = temp;
+//                 start++;
+//                 end--;
+//             }
+//         }
+        
+//         quickSort(arr, low, end);
+//         quickSort(arr, start, high);
+//     }
 }
