b32f738ad7c1d6cc65a414a3d60150fdc7207f55
Santhosh Kumar Thoomatti <thoomatti.santhosh.w@gmail.com>
2022-07-01 11:39:14 +0530

    Time: 1 ms (50.60%), Space: 40.7 MB (63.69%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -7,7 +7,20 @@ class Solution {
         
         dp[0][0] = 1;
         
-        return f(m-1, n-1, dp);
+        for(int r = 0; r < m; r++){
+            for(int c = 0; c < n; c++){
+                if(r == 0 && c == 0) dp[r][c] = 1;
+                else{
+                    int ans = 0;
+                    if(r > 0) ans += dp[r-1][c];
+                    if(c > 0) ans += dp[r][c-1];
+                    dp[r][c] = ans;
+                }
+            }
+        }
+        
+        return dp[m-1][n-1];
+        // return f(m-1, n-1, dp);
     }
     
     public int f(int m, int n, int[][] dp){
