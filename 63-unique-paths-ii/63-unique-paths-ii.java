3cd39c4379fb4765c2404c7d156bb093f2d9e9d9
Santhosh Kumar Thoomatti <thoomatti.santhosh.w@gmail.com>
2022-07-02 17:32:01 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (89.05%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -4,25 +4,32 @@ class Solution {
         int r = obstacleGrid.length;
         int c = obstacleGrid[0].length;
         
+        // DP array to store values
         int[][] dp = new int[r][c];
         for(int[] row: dp)
             Arrays.fill(row, -1);
-        
         
         return f(r-1, c-1, obstacleGrid, dp);
     }
     
     public int f(int r, int c, int[][] grid, int[][] dp){
+        // condition to skip obstacle cell
         if(grid[r][c] == 1) return 0;
         
+        // when reached (0,0), a unique path is found so return 1
         if(r == 0 && c == 0) return 1;
     
+        // check whether DP array got a result for (r,c)
+        // if exist return the result
         if(dp[r][c] != -1) return dp[r][c];
         
+        // else calculate and update the result in DP and return
         int ans = 0;
         
+        // to move down
         if(r > 0) ans += f(r-1, c, grid, dp);
         
+        // to move right
         if(c > 0) ans += f(r, c-1, grid, dp);
         
         return dp[r][c] = ans;
