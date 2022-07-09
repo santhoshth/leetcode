e7ca3312a228993f801451fe241af4bb981f792e
Santhosh Kumar Thoomatti <thoomatti.santhosh.w@gmail.com>
2022-07-09 12:00:14 +0530

    Time: 2 ms (78.23%), Space: 52.2 MB (65.23%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum = 0;
+        for(int each: nums){
+            sum += each;
+        }
+        
+        int leftSum = 0;
+        for(int i = 0; i < nums.length; i++){
+            sum -= nums[i];
+            if(leftSum == sum) return i;
+            leftSum += nums[i];
+        }
+        
+        return -1;
+    }
+}
