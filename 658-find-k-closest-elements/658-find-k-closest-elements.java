cac78d6601f457b497743d72457892104386de7e
Santhosh Kumar Thoomatti <thoomatti.santhosh.w@gmail.com>
2022-07-09 13:35:22 +0530

    Time: 4 ms (93.89%), Space: 61.7 MB (79.56%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,52 +1,34 @@
 class Solution {
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int ans = getIndex(arr, x);
-        System.out.println(ans);
-        List<Integer> res = new ArrayList<>();
-        int n = arr.length;
-        int i = ans;
-        int j = ans;
-        
-        while(k >= 0 && i >= 0 && j < n){
-            if(x - arr[i] <= arr[j] - x){
-                i--;
+    public List<Integer> findClosestElements(int[] nums, int k, int x) {
+        int lo = 0;
+        int hi = nums.length - 1;
+        while(lo < hi) {
+            int mid = lo + (hi - lo)/2;
+            if(nums[mid] < x) {
+                lo = mid + 1;
             } else {
-                j++;
-            } 
-            k--;
-        }
-        
-        while(k >= 0 && i >= 0){
-            i--;
-            k--;
-        }
-        
-        while(k >= 0 && j < n){
-            j++;
-            k--;
-        }
-        
-        for(int low = i+1; low < j; low++){
-            res.add(arr[low]);
-        }
-
-        return res;
-    }
-    
-    public int getIndex(int[] nums, int target){
-        int s = 0;
-        int e = nums.length - 1;
-        
-        while(s < e){
-            int m = s + (e-s)/2;
-            
-            if(nums[m] < target){
-                s = m+1;
-            } else {
-                e = m;
+                hi = mid;
             }
         }
         
-        return s;
+        
+        int start = lo;
+        int end = lo;
+        int count = 0;
+        while(count <= k && start >= 0 && end < nums.length) {
+            if(x - nums[start] > nums[end] - x) end++;
+            else start--;
+            count++;
+        }
+        
+        while(count <= k && start >= 0) { start--; count++; }
+        while(count <= k && end < nums.length) { end++; count++; }
+        
+        List<Integer> result = new ArrayList<>();
+        for(int i=start+1; i<end; i++) {
+            result.add(nums[i]);
+        }
+        
+        return result;
     }
 }
