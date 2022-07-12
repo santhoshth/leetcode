f05f309f8a225cd78de18bc84f1a794791805129
Santhosh Kumar Thoomatti <thoomatti.santhosh.w@gmail.com>
2022-07-12 19:22:49 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (54.46%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
@@ -13,6 +13,10 @@ class Solution {
         ListNode slow = head;
         ListNode fast = head;
         
+        // using fast and slow pointers 
+        // slow pointer incremented by 1 node
+        // fast pointer incremented by 2 nodes
+        // when the while loop breaks slow pointer will be middle node
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
