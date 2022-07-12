b3f10ff1ef120907d06505cf7d11f21176ae0a30
Santhosh Kumar Thoomatti <thoomatti.santhosh.w@gmail.com>
2022-07-12 19:22:08 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (49.74%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -9,38 +9,38 @@
  * }
  */
 class Solution {
-    public ListNode reverseList(ListNode head) {
-        if(head == null || head.next == null) {
-           return head;
-       }
-        ListNode newHead = reverseList(head.next);
-        head.next.next = head;
-        head.next = null;
-        
-        return newHead;
-    }
 //     public ListNode reverseList(ListNode head) {
-//         if(head == null){
-//             return head;
-//         }
+//         if(head == null || head.next == null) {
+//            return head;
+//        }
+//         ListNode newHead = reverseList(head.next);
+//         head.next.next = head;
+//         head.next = null;
         
-//         if(head != null && head.next == null){
-//             return head;
-//         }
-        
-//         // using 3 pointer method
-//         ListNode previous = null;
-//         ListNode present = head;
-//         ListNode next = head.next;
-        
-//         while(next != null){
-//             present.next = previous;
-//             previous = present;
-//             present = next;              
-//             next = next.next;
-//         }
-//         present.next = previous;
-        
-//         return present;
+//         return newHead;
 //     }
+    public ListNode reverseList(ListNode head) {
+        if(head == null){
+            return head;
+        }
+        
+        if(head != null && head.next == null){
+            return head;
+        }
+        
+        // using 3 pointer method
+        ListNode previous = null;
+        ListNode present = head;
+        ListNode next = head.next;
+        
+        while(next != null){
+            present.next = previous;
+            previous = present;
+            present = next;              
+            next = next.next;
+        }
+        present.next = previous;
+        
+        return present;
+    }
 }
